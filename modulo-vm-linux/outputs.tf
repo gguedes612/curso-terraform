1585a292ec2373b05714ca7f3620972eeeedc8c1
Guilherme Guedes <gguedes612@gmail.com>
2022-02-08 04:48:05 -0300

    Adicionando outputs DNS

--- modulo-vm-linux/outputs.tf
+++ modulo-vm-linux/outputs.tf
@@ -2,3 +2,13 @@ output "public_ip_address" {
     value = module.vm-linux.public_ip_address
     description = "IP publico para a interface da Maquina Virtual"
 }
+
+output "dns_name_server" {
+    value = module.vm-linux.dns_name_server
+    description = "Servidores DNS Azure"
+}
+
+output "dominio_servidor" {
+    value = module.vm-linux.dominio_servidor
+    description = "Dominio para entrar no servidor"
+}
