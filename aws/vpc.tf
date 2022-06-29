00eacb5c77c668aadf7033e9e92870b0cd167998
Guilherme Guedes <g.guedes@gmail.com>
2022-06-29 17:48:26 -0300

    Adicionando modulo cluster_aws

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -1,21 +1,41 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.20.1"
-    }
-  }
+variable "region" {
+  default     = "us-east-2"
+  description = "AWS region"
+}
+
+variable "vpc_id" {
+  default = "vpc-0df31a6e2d113d8b7"
+  description = "ID do VPC que sera utilizado nesse terraform"
+}
+
+variable "subnet_ids" {
+  type = list
+  default = ["subnet-04f9462620240c5c1","subnet-00c7176f1c6f72c32","subnet-0961dd9a7d368e1b0"]
+  description = "Ids da Subnet que sera utilizado nesse terraform"
 }
 
 provider "aws" {
-    region = "us-east-2"
+  region = var.region
 }
 
-#####
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+data "aws_availability_zones" "available" {}
+
+
+data "aws_vpc" "main" {
+  id = var.vpc_id
 }
 
-output "aws_vpc_id" {
-  value = aws_vpc.main.id
+data "aws_subnet" "main" {
+  count = length(var.subnet_ids)
+  id = var.subnet_ids["${count.index}"]
+}
+
+
+output "vpc_cidr" {
+  value = data.aws_vpc.main.cidr_block
+  description = "Bloco cidr da VPC"
+}
+
+output "subnets_cidr" {
+  value = [for subnets in data.aws_subnet.main: subnets.cidr_block]
 }
